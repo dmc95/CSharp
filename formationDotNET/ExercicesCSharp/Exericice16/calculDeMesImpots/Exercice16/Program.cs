d0d8fe6a410d83131af967640b0ed8bec850dfc3
dmc95 <d-mc95@hotmail.fr>
2021-12-10 14:22:57 +0100

    création du repo

---
+++ formationDotNET/ExercicesCSharp/Exericice16/calculDeMesImpots/Exercice16/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Exercice16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--- Quelle sera le montant de mes impôts ? ---");
+            Console.Write("\nEntrez le montant net imposable du foyer (en Euros) :");
+            int netImposable = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Entrez le nombre d'adulte du foyer :");
+            int nbreAdulte = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Entrez le nombre d'enfants du foyer :");
+            int nbreEnfant = Convert.ToInt32(Console.ReadLine());
+
+            #region Structure Conditionelle
+
+
+
+            #endregion
+        }
+    }
+}
